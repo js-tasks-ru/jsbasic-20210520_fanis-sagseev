9152ffbcd9494961691f541a513388078ffa28f1
Fanis Sagdeev <fanis.maker@gmail.com>
2021-05-29 16:04:01 +0300

    Add sum salary sum Function

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let pay = 0;
+  
+  for(let prop in salaries) {
+    let salaryValue = salaries[prop];
+    if (isFinite(salaryValue)) {
+      pay += salaryValue;
+    }
+  }
+  return pay;
 }
