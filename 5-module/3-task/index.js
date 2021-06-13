c4446a17ef95b5554c269dc87d7b2ddf96d58785
Fanis Sagdeev <fanis.maker@gmail.com>
2021-06-13 19:51:18 +0300

    Carousel Navigation

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,33 @@
 function initCarousel() {
-  // ваш код...
+  let innerCarousel = document.querySelector ('.carousel__inner');
+  let slides = document.querySelectorAll ('.carousel__slide');
+  let slideWidth = innerCarousel.offsetWidth;
+  let slideNumber = 1;
+  let rightNavArrow = document.querySelector ('.carousel__arrow_right');
+  let leftNavArrow = document.querySelector ('.carousel__arrow_left');
+
+leftNavArrow.style.display ='none';
+rightNavArrow.addEventListener ('click', () => {
+  innerCarousel.style.transform = `translateX(-${slideWidth * slideNumber++ + 'px'})`;
+    if (slideNumber > slides.length -1) {
+      rightNavArrow.style.display = 'none';
+    } else {
+      rightNavArrow.style.display = '';
+    }
+    if (slideNumber > 1) {
+      leftNavArrow.style.display = '';
+    }
+});
+
+leftNavArrow.addEventListener ('click', () => {
+  innerCarousel.style.transform = `translateX(-${slideWidth * (--slideNumber -1) + 'px'})`;
+  if (slideNumber > slides.length -1) {
+    rightNavArrow.style.display = 'none';
+  } else {
+    rightNavArrow.style.display = '';
+  }
+  if (slideNumber == 1) {
+    leftNavArrow.style.display = 'none';
+  }
+});
 }
